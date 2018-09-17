c14c7b5b3c6a398813ff8fb044768a2143fbb7f4
Holly Hardin <Holly@hollysmacintosh.lan>
2018-09-17 08:12:30 -0700

    Update PNMwriter.C

--- ImageFilters/PNMwriter.C
+++ ImageFilters/PNMwriter.C
@@ -2,17 +2,17 @@
 
 void PNMwriter::Write(const char *f)
 {
-    // Open file
-    FILE *f_out = fopen(f, "wb");
-    
-    // Get image's dimensions
-    const int width = image->GetWidth();
-    const int height = image->GetHeight();
+	// Open file
+	FILE *f_out = fopen(f, "wb");
 
-    // Write header values and pixel map
-    fprintf(f_out, "P6\n%d %d\n255\n", width, height);
-    fwrite(image->GetPixmap(), 1, 3 * width * height, f_out);
+	// Get image's dimensions
+	const int width = image->GetWidth();
+	const int height = image->GetHeight();
 
-    // Close file
-    fclose(f_out);
+	// Write header values and pixel map
+	fprintf(f_out, "P6\n%d %d\n255\n", width, height);
+	fwrite(image->GetPixmap(), 1, 3 * width * height, f_out);
+
+	// Close file
+	fclose(f_out);
 };
