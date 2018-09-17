b20554e1fb388a77e9619b3ea4a7efe83431d195
Holly Hardin <Holly@hollysmacintosh.lan>
2018-09-17 08:06:00 -0700

    Update PNMreader.h

--- ImageFilters/PNMreader.h
+++ ImageFilters/PNMreader.h
@@ -5,19 +5,19 @@
 class PNMreader : public Source
 {
     public:
-        // Constructor and Destructor
-	    PNMreader(const char *f);
-	    ~PNMreader()            { delete [] filename; };
+	// Constructor and Destructor
+	PNMreader(const char *f);
+	~PNMreader()			  { delete [] filename; };
 
-        // Method for getting Source name
-        const char *SourceName()    { return "PNMreader"; };
-	
-        // Method for reading image
-	    void Execute();
+	// Method for getting Source name
+	const char *SourceName()	  { return "PNMreader"; };
 
-        // Variables
-        char *filename;
-        char magicNum[128];
-        int width, height;
+	// Method for reading image
+	void Execute();
+
+	// Variables
+	char *filename;
+	char magicNum[128];
+	int width, height;
 	char maxval[128];
 };
