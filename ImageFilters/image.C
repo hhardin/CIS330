0e1bcd448aa50ade728c9c179be8c258b3ff1d4e
Holly Hardin <Holly@hollysmacintosh.lan>
2018-09-17 10:04:04 -0700

    Update image.C

--- ImageFilters/image.C
+++ ImageFilters/image.C
@@ -4,41 +4,41 @@
 
 Image::Image()
 {
-    // Initialize variables
+	// Initialize variables
 	width = 0;
 	height = 0;
 	Pixmap = NULL;
-    source = NULL;
+	source = NULL;
 }
 
 void Image::SetPixmap(Pixel *pxl)
 {
-    int i, j;
-    for (i = 0; i < height; i++)
-        for (j = 0; j < width; j++)
-        {
-            Pixmap[i*width + j].r = pxl[i*width + j].r;
-            Pixmap[i*width + j].g = pxl[i*width + j].g;
-            Pixmap[i*width + j].b = pxl[i*width + j].b;
-        }
+	int i, j;
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+		{
+			Pixmap[i*width + j].r = pxl[i*width + j].r;
+			Pixmap[i*width + j].g = pxl[i*width + j].g;
+			Pixmap[i*width + j].b = pxl[i*width + j].b;
+		}
 }
 
 void Image::Reset(int w, int h)
 {
-    // Reset variables
+	// Reset variables
 	width = w;
 	height = h;
-    
-    // Delete pixel info
-    delete [] Pixmap;
 
-    // Create new pixmap
+	// Delete pixel info
+	delete [] Pixmap;
+
+	// Create new pixmap
 	Pixmap = new Pixel[w*h];
 }
 
 void Image::Update() const 
 {
-    // Request the source pointer to do an update 
-    if (source != NULL)
-        source->Update();
+	// Request the source pointer to do an update 
+	if (source != NULL)
+		source->Update();
 };
