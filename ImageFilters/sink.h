f2c9a6acd1e51f08d9d8614486da7f7b65ea759c
Holly Hardin <Holly@hollysmacintosh.lan>
2018-09-17 07:49:01 -0700

    Update sink.h

--- ImageFilters/sink.h
+++ ImageFilters/sink.h
@@ -8,20 +8,20 @@
 class Sink
 {
     public:
-        // Constructor
-	    Sink();
+	// Constructor
+	Sink();
 
-        // Methods to set input of images
-	    void SetInput(Image* img); 
-	    void SetInput2(Image* img);
+	// Methods to set input of images
+	void SetInput(Image* img);
+	void SetInput2(Image* img);
 
-        // Method for getting sink name
-        virtual const char *SinkName() = 0;
+	// Method for getting sink name
+	virtual const char *SinkName() = 0;
 
     protected:
-        // Variables
-    	const Image *image;
-    	const Image *image2;
+	// Variables
+	const Image *image;
+	const Image *image2;
 };
 
 #endif
