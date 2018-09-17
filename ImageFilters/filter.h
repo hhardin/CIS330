aa8a017b125c677f7e0196c5c632fe0245706f64
Holly Hardin <Holly@hollysmacintosh.lan>
2018-09-17 10:01:40 -0700

    Update filter.h

--- ImageFilters/filter.h
+++ ImageFilters/filter.h
@@ -5,140 +5,140 @@
 class Color : public Source
 {
     public:
-        // Constructor
-        Color(int w, int h, unsigned char r, unsigned char g, unsigned char b);
+	// Constructor
+	Color(int w, int h, unsigned char r, unsigned char g, unsigned char b);
 
-        // Method for getting source name
-        const char *SourceName()                 { return "Color"; };
+	// Method for getting source name
+	const char *SourceName()		{ return "Color"; };
 
-        // Method for creating a colored image
-        void Execute();
+	// Method for creating a colored image
+	void Execute();
 
     protected:
-        int height, width;
-        unsigned char red, blue, green;
+	int height, width;
+	unsigned char red, blue, green;
 };
 
 class CheckSum : public Sink
 {
     public:
-        // Method for getting sink name
-        const char *SinkName()                   { return "CheckSum"; };
+	// Method for getting sink name
+	const char *SinkName()			{ return "CheckSum"; };
 
-        // Method for summing the total value of the colors
-        void OutputCheckSum(const char *f);
+	// Method for summing the total value of the colors
+	void OutputCheckSum(const char *f);
 };
 
 class Filter : public Source, public Sink
 {
     public:
-        // Filter
-        Filter();
+	// Filter
+	Filter();
 
-        // Method for updating image pointer 
-        virtual void Update();
+	// Method for updating image pointer 
+	virtual void Update();
 
-        // Method for getting filter name
-        virtual const char *FilterName() = 0;
-        const char *SourceName()                 { return FilterName(); };
-        const char *SinkName()                   { return FilterName(); };
+	// Method for getting filter name
+	virtual const char *FilterName() = 0;
+	const char *SourceName()		{ return FilterName(); };
+	const char *SinkName()			{ return FilterName(); };
 
-        // Variable for updating the image
-        bool updating;
+	// Variable for updating the image
+	bool updating;
 };
 
 class Mirror : public Filter
 {
     public:
-        // Method for mirroring image
-        void Execute();
+	// Method for mirroring image
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Mirror"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Mirror"; };
 };
 
 class Rotate : public Filter
 {
     public:
-        // Method for rotating image 90 degrees
-        void Execute();
+	// Method for rotating image 90 degrees
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Rotate"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Rotate"; };
 };
 
 class Subtract : public Filter
 {
     public:
-        // Method for finding the difference between two images
-        void Execute();
+	// Method for finding the difference between two images
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Subtract"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Subtract"; };
 };
 
 class Grayscale : public Filter
 {
     public:
-        // Method for removing the color from an image
-        void Execute();
+	// Method for removing the color from an image
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Grayscale"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Grayscale"; };
 };
 
 class Blur : public Filter
 {
     public:
-        // Method for blurring an image
-        void Execute();
+	// Method for blurring an image
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Blur"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Blur"; };
 };
 
 class Shrinker : public Filter
 {
     public:
-        // Method for shrinking image
-	    void Execute();
+	// Method for shrinking image
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Shrinker"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Shrinker"; };
 };
 
 
 class LRCombine : public Filter
 {
     public:
-        // Method for combining left and right images
-    	void Execute();
-        
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "LRConcat"; };
+	// Method for combining left and right images
+	void Execute();
+
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "LRConcat"; };
 };
 
 class TBCombine : public Filter
 {
     public:
-        // Method for combining top and bottom images
-        void Execute();
+	// Method for combining top and bottom images
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "TBConcat"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "TBConcat"; };
 };
 
 class Blender : public Filter
 {
     public:
-        // Methods for blending image
-	    void SetFactor(double f)                { factor = f; };
-	    void Execute();
+	// Methods for blending image
+	void SetFactor(double f)		{ factor = f; };
+	void Execute();
 
-        // Method for getting Source name
-        virtual const char *FilterName()         { return "Blender"; };
+	// Method for getting Source name
+	virtual const char *FilterName()	{ return "Blender"; };
 
-        // Variables
-    	double factor;
-        double remaining;
+	// Variables
+	double factor;
+	double remaining;
 };
